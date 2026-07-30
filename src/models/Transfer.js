2841567f0ba2852cffc92586eb1d8095c69afc25
dimmimik <dimmimik@ukr.net>
2026-07-30 22:05:12 +0300

    Create Transfer class for handling transactions

---
+++ src/models/Transfer.js
@@ -0,0 +1,15 @@
+class Transfer {
+
+    constructor(id, from, to, amount, fee) {
+
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+        this.fee = fee;
+
+    }
+
+}
+
+module.exports = Transfer;
