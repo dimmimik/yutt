0fe14f4f0c1072f4a82d5d33227659cb8a06f986
dimmimik <dimmimik@ukr.net>
2026-07-30 22:04:30 +0300

    Create networks.js

---
+++ config/networks.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    supportedNetworks: [
+
+        "Ethereum",
+        "Base",
+        "Arbitrum",
+        "Polygon"
+
+    ],
+
+    stablecoin: "USDC",
+
+    visualization: {
+
+        showAmounts: true,
+        showRoutes: true,
+        showFees: true
+
+    }
+
+};
